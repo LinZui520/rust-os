494173065898c43258de824ba66bb51b55b0e538
linzui <yangminghe20@gmail.com>
2023-10-25 22:59:47 +0800

    实现write宏

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 cargo bootimage --release
-qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/release/bootimage-rust_os.bin
+
+if [ $? -eq 0 ];
+then 
+    qemu-system-x86_64 -drive format=raw,file=target/x86_64-rust_os/release/bootimage-rust_os.bin
+else
+    echo "镜像构建失败"
+fi
